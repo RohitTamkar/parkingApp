7a4c6660cffcb732485c1f8561d5330780aed57f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-12-11 13:00:33 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/parking/parking_check_i_n/parking_check_i_n_widget.dart
+++ lib/pages/parking/parking_check_i_n/parking_check_i_n_widget.dart
@@ -1413,15 +1413,8 @@ class _ParkingCheckINWidgetState extends State<ParkingCheckINWidget> {
                                               0.0,
                                             ),
                                             taxAmt: 0.0,
-                                            billAmt: valueOrDefault<double>(
-                                              FFAppState().parkingCharges,
-                                              0.0,
-                                            ),
-                                            finalBillAmt:
-                                                valueOrDefault<double>(
-                                              FFAppState().parkingCharges,
-                                              0.0,
-                                            ),
+                                            billAmt: 0.0,
+                                            finalBillAmt: 0.0,
                                             roundOff: 0.0,
                                             shiftId: getJsonField(
                                               widget!.shiftDoc,
@@ -1474,15 +1467,8 @@ class _ParkingCheckINWidgetState extends State<ParkingCheckINWidget> {
                                               0.0,
                                             ),
                                             taxAmt: 0.0,
-                                            billAmt: valueOrDefault<double>(
-                                              FFAppState().parkingCharges,
-                                              0.0,
-                                            ),
-                                            finalBillAmt:
-                                                valueOrDefault<double>(
-                                              FFAppState().parkingCharges,
-                                              0.0,
-                                            ),
+                                            billAmt: 0.0,
+                                            finalBillAmt: 0.0,
                                             roundOff: 0.0,
                                             shiftId: getJsonField(
                                               widget!.shiftDoc,
