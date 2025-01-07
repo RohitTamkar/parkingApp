bb10da68bc202ae66fc70089439f6d273325b779
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-07 09:33:17 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/parking/todays_summary_report/todays_summary_report_model.dart
+++ lib/pages/parking/todays_summary_report/todays_summary_report_model.dart
@@ -1,8 +1,11 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/api_requests/api_calls.dart';
 import '/backend/backend.dart';
 import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/pages/parking/email_input/email_input_widget.dart';
 import 'dart:ui';
 import '/custom_code/actions/index.dart' as actions;
 import '/flutter_flow/custom_functions.dart' as functions;
@@ -18,6 +21,10 @@ class TodaysSummaryReportModel
     extends FlutterFlowModel<TodaysSummaryReportWidget> {
   ///  State fields for stateful widgets in this page.
 
+  // Stores action output result for [Custom Action - genExcelForTodaySummaryReport] action in IconButton widget.
+  String? base64Link2;
+  // Stores action output result for [Backend Call - API (sendMail)] action in IconButton widget.
+  ApiCallResponse? apiResult6yc2;
   DateTime? datePicked1;
   DateTime? datePicked2;
   // Stores action output result for [Custom Action - returnDateNextday] action in Row widget.
