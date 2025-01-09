e54fafc86df4f23f42892b1c24093eec6fe52d32
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-09 06:46:17 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/parking/parking_check_i_n/parking_check_i_n_widget.dart
+++ lib/pages/parking/parking_check_i_n/parking_check_i_n_widget.dart
@@ -715,7 +715,9 @@ class _ParkingCheckINWidgetState extends State<ParkingCheckINWidget> {
                                               controller: _model
                                                       .dropDownvechicleValueController ??=
                                                   FormFieldController<String>(
-                                                      null),
+                                                _model.dropDownvechicleValue ??=
+                                                    _model.vehicleType,
+                                              ),
                                               options:
                                                   dropDownvechicleProductRecordList
                                                       .map((e) => e.name)
@@ -1739,11 +1741,8 @@ class _ParkingCheckINWidgetState extends State<ParkingCheckINWidget> {
                                             safeSetState(() {
                                               _model.textController1?.clear();
                                             });
-                                            safeSetState(() {
-                                              _model
-                                                  .dropDownvechicleValueController
-                                                  ?.value = _model.vehicleType!;
-                                            });
+                                            _model.vehicleType = null;
+                                            safeSetState(() {});
                                             if (_shouldSetState)
                                               safeSetState(() {});
                                             return;
