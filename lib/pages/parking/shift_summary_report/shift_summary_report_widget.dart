7352f6c3ab8b2b2712f73331cfc5aead797640e3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-20 12:57:24 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/parking/shift_summary_report/shift_summary_report_widget.dart
+++ lib/pages/parking/shift_summary_report/shift_summary_report_widget.dart
@@ -640,7 +640,7 @@ class _ShiftSummaryReportWidgetState extends State<ShiftSummaryReportWidget> {
                                                                               widget!
                                                                                   .docRef!.startTime) &&
                                                                           (e.checkInTime <=
-                                                                              widget!.docRef!.startTime)))
+                                                                              widget!.docRef!.endTime)))
                                                                   .toList()
                                                                   .length
                                                                   .toString(),
@@ -707,11 +707,9 @@ class _ShiftSummaryReportWidgetState extends State<ShiftSummaryReportWidget> {
                                                                       (e.vechicleType ==
                                                                           listItem
                                                                               .vechicleType) &&
-                                                                      ((e.checkOutTime >=
-                                                                              widget!
-                                                                                  .docRef!.startTime) &&
+                                                                      ((e.checkOutTime >= FFAppState().startDate!.millisecondsSinceEpoch) &&
                                                                           (e.checkOutTime <=
-                                                                              widget!.docRef!.startTime)))
+                                                                              FFAppState().endDate!.millisecondsSinceEpoch)))
                                                                   .toList()
                                                                   .length
                                                                   .toString(),
